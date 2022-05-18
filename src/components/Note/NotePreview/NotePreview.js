c5322c9474bea3befad13406a374a34cd117e3b4
unknown <oliwerklauze22@gmail.com>
2022-05-18 22:26:13 +0200

    selecting some features added

--- src/components/Note/NotePreview/NotePreview.js
+++ src/components/Note/NotePreview/NotePreview.js
@@ -1,45 +1,49 @@
 import React, { useContext, useState } from 'react';
 import AppContext from 'store/AppContext';
 import './NotePreview.css';
-export default function NotePreview({ title, date, color, ...props }) {
+export default function NotePreview({ title, date, color, id, ...props }) {
   const { ua, language } = useContext(AppContext);
   const [isSelectMode, setIsSelectMode] = useState(false);
   const [selectedNotes, setselectedNotes] = useState([]);
   //TODO Change for reducer
 
-  const selectNote = (e) => {
+  const selectNote = (e, noteId) => {
     console.log('select');
+    e.currentTarget.classList.toggle('selectedNote');
+
+    const temp = [...selectedNotes, noteId];
+    setselectedNotes(temp);
+    console.log(selectedNotes);
   };
   const handleOpenFullView = () => {
     console.log('full view');
   };
 
   let isPressed = false;
-  const handleTimeout = (item) => {
+  const handleTimeout = (e, noteId) => {
     if (isPressed) {
-      item.classList.toggle('selectedNote');
+      selectNote(e, noteId);
     } else {
       handleOpenFullView();
     }
   };
-  const MobileTouchStart = (e) => {
+  const MobileTouchStart = (e, noteId) => {
     isPressed = true;
     if (e.currentTarget.className.includes('selectedNote')) {
       e.currentTarget.classList.remove('selectedNote');
     } else {
-      window.setTimeout(handleTimeout, 250, e.currentTarget);
+      window.setTimeout(handleTimeout, 250, e, noteId);
     }
   };
   const MobileTouchEnd = () => {
     isPressed = false;
     window.clearTimeout(handleTimeout);
   };
-
-  const handleClick = (e) => {
+  const handleClick = (e, noteId) => {
     if (ua === 'mobile' || ua === 'tablet') return;
 
     if (e.shiftKey || e.altKey || e.ctrlKey) {
-      e.currentTarget.classList.toggle('selectedNote');
+      selectNote(e, noteId);
     } else if (!isSelectMode) {
       handleOpenFullView();
     }
@@ -50,8 +54,8 @@ export default function NotePreview({ title, date, color, ...props }) {
       className="notePreview"
       style={{ backgroundColor: `var(--noteColor-${color})` }}
       {...props}
-      onClick={handleClick}
-      onTouchStart={MobileTouchStart}
+      onClick={(e) => handleClick(e, id)}
+      onTouchStart={(e) => MobileTouchStart(e, id)}
       onTouchEnd={MobileTouchEnd}
     >
       <h1 className="notePreviewTitle">{title}</h1>
