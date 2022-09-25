8bc7624d665fd6a7518b6f64a5963a32e0745874
unknown <oliwerklauze22@gmail.com>
2022-09-25 15:51:29 +0200

    long press support add

--- src/components/Note/NotePreview/NotePreview.js
+++ src/components/Note/NotePreview/NotePreview.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import AppContext from 'store/AppContext';
 import { LongPressDetectEvents, useLongPress } from 'use-long-press';
@@ -6,6 +6,7 @@ import handleOpenFullView from 'utils/handleOpenFullView';
 import './NotePreview.css';
 
 export default function NotePreview({
+  key,
   title,
   date,
   color,
@@ -16,70 +17,38 @@ export default function NotePreview({
 }) {
   const { ua, language } = useContext(AppContext);
 
-  const selectNote = () => {
-    console.log(notesState.selectedNotes, `Current Id: ${id}`);
+  const [enabled, setEnabled] = useState(true);
+
+  const selectNote = (n) => {
     const temp = notesState.selectedNotes;
-    if (temp[id]) {
-      delete temp[id];
+    if (temp[n]) {
+      delete temp[n];
     } else {
-      temp[id] = id;
+      temp[n] = n;
     }
     setNotesState({ ['selectedNotes']: temp });
   };
 
-  // const [enabled, setEnabled] = useState(true);
-  // // let currentId;
-  // const callback = useCallback(() => {
-  //   if (ua === 'desktop') {
-  //     console.log('callback pc');
-  //   } else {
-  //     console.log('callback mobile');
-  //   }
-  //   // selectNote(currentId);
-  // }, []);
-  // const bind = useLongPress(enabled ? callback : null, {
-  //   onStart: (e) => {
-  //     const isSelectedMode = Object.keys(notesState.selectedNotes).length > 0;
+  const callback = () => {
+    if (ua === 'desktop') {
+      handleOpenFullView(setNotesState, id);
+    } else {
+      selectNote(id);
+    }
+  };
 
-  //     if (ua === 'desktop') {
-  //       console.log(
-  //         'start pc',
-  //         parseFloat(e.currentTarget.getAttribute('data-id')),
-  //       );
-  //       handleOpenFullView(
-  //         setNotesState,
-  //         parseFloat(e.currentTarget.getAttribute('data-id')),
-  //       );
-  //     } else if (isSelectedMode) {
-  //       console.log('start mobile', 'add instnatly and do not run callback');
-  //     } else {
-  //       console.log('default start mobile');
-  //     }
-  //     // console.log('Press started', e.currentTarget);
-  //     // currentId = parseFloat(e.currentTarget.getAttribute('data-id'));
-  //   },
-  //   // onFinish: (e) => console.log('Long press finished'),
-  //   onCancel: (e) => {
-  //     if (ua === 'desktop') {
-  //       console.log('cancel pc', e.currentTarget);
-  //     } else {
-  //       handleOpenFullView(
-  //         setNotesState,
-  //         parseFloat(e.currentTarget.getAttribute('data-id')),
-  //       );
-  //       console.log('cancel mobile', e.currentTarget);
-  //     }
-  //   },
-  //   onMove: () => console.log('Detected mouse or touch movement'),
-  //   threshold: 1000,
-  //   captureEvent: true,
-  //   cancelOnMovement: true,
-  //   detect: LongPressDetectEvents.BOTH,
-  // });
+  const bind = useLongPress(enabled ? callback : null, {
+    onStart: () => console.log('Press started'),
+    onFinish: () => console.log('Long press finished'),
+    onCancel: () => console.log('Press cancelled'),
+    //onMove: () => console.log("Detected mouse or touch movement"),
+    threshold: 500,
+    captureEvent: true,
+    cancelOnMovement: true,
+    detect: LongPressDetectEvents.BOTH,
+  });
 
   const handleClick = (e) => {
-    console.log('click exexuction');
-    // if (ua === 'mobile' || ua === 'tablet') return;
     const isSelectedMode = Object.keys(notesState.selectedNotes).length > 0;
     if (e.shiftKey || e.altKey || e.ctrlKey || isSelectedMode) {
       selectNote(id);
@@ -93,9 +62,8 @@ export default function NotePreview({
         notesState.selectedNotes[id] && 'selectedNote'
       }`}
       style={{ backgroundColor: `var(--noteColor-${color})` }}
-      // data-id={id}
       onClick={handleClick}
-      // {...bind()}
+      {...bind()}
       {...props}
     >
       <span
